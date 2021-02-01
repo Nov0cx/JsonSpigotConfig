5b28e4a5a7edfdc72eed2e004c5676e42ea4097d
Nov0cx <niklas.leyraf44@gmail.com>
2021-02-01 07:01:15 +0100

    writing in JsonConfig

--- src/main/kotlin/com/nov0cx/jsonspigotconfig/spigot/JsonConfig.kt
+++ src/main/kotlin/com/nov0cx/jsonspigotconfig/spigot/JsonConfig.kt
@@ -2,6 +2,8 @@ package com.nov0cx.jsonspigotconfig.spigot
 
 import com.nov0cx.jsonspigotconfig.json.Json
 import java.io.File
+import java.math.BigDecimal
+import java.math.BigInteger
 
 /*
  * <p>
@@ -29,5 +31,46 @@ class JsonConfig() {
     fun writeClass(clazz: Class<Any>) {
         json.writeObject(file, clazz)
     }
+
+    fun writeString(file: File, name: String, value: String) {
+        json.writeString(file, name, value)
+    }
+
+    fun writeInt(file: File, name: String, value: Int) {
+        json.writeInt(file, name, value)
+    }
+
+    fun writeFloat(file: File, name: String, value: Float) {
+        json.writeFloat(file, name, value)
+    }
+
+    fun writeDouble(file: File, name: String, value: Double) {
+        json.writeDouble(file, name, value)
+    }
+
+    fun writeLong(file: File, name: String, value: Long) {
+        json.writeLong(file, name, value)
+    }
+
+    fun writeShort(file: File, name: String, value: Short) {
+        json.writeShort(file, name, value)
+    }
+
+    fun writeByteArray(file: File, name: String, value: ByteArray) {
+        json.writeByteArray(file, name, value)
+    }
+
+    fun writeBool(file: File, name: String, value: Boolean) {
+        json.writeBool(file, name, value)
+    }
+
+    fun writeBigDecimal(file: File, name: String, value: BigDecimal) {
+        json.writeBigDecimal(file, name, value)
+    }
+
+    fun writeBigInteger(file: File, name: String, value: BigInteger) {
+        json.writeBigInteger(file, name, value)
+    }
+
 
 }
